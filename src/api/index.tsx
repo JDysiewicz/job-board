33aae664bdfd5d131ab016f4d795a5b44c91bb67
JDysieiwcz <joshuadysiewicz@googlemail.com>
2021-12-20 14:58:15 +0000

    fixed base url

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -2,6 +2,6 @@ import ApiProvider from "./ApiProvider";
 
 // different endpoint for dev/prod
 // not necessary in this case but extensible
-const endpoint = process.env.REACT_APP_API_HOST || "https://api.graphql.jobs/";
+const endpoint = process.env.REACT_APP_API_HOST || "";
 
 export { ApiProvider, endpoint };
